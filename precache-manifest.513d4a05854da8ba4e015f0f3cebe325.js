cc26fce7b4b84a5499dc527c8ab58f499f8bdb93
Iván Quevedo <iquevedo@abalit.org>
2019-06-17 09:55:32 +0200

    Updates

---
+++ precache-manifest.513d4a05854da8ba4e015f0f3cebe325.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fc7e856f475a2671f442e1089e48e1",
+    "url": "/colored-map/index.html"
+  },
+  {
+    "revision": "9796d77314e22dab9f4c",
+    "url": "/colored-map/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "727633f6145a796b6c8e",
+    "url": "/colored-map/static/js/2.e926d808.chunk.js"
+  },
+  {
+    "revision": "9796d77314e22dab9f4c",
+    "url": "/colored-map/static/js/main.ef1acd9f.chunk.js"
+  },
+  {
+    "revision": "54f6e7ea505d0550a9bd",
+    "url": "/colored-map/static/js/runtime~main.d37b7aee.js"
+  }
+]);
