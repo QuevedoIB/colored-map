15ba9b70fbf62c6f37b6035bc19aa05e943f8e8c
QuevedoIB <45363951+QuevedoIB@users.noreply.github.com>
2019-09-17 10:09:43 +0200

    Update index.js

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,16 +1,4 @@
-export const GET_VISITED_COUNTRIES = idUser => {
-  return (dispatch) => {
-    alert(`Llamada api a ${idUser}`)
-    //api call
-    return dispatch({
-      type: "GET_VISITED_COUNTRIES", payload: {
-        ES: 1,
-        FR: 1,
-        DE: 1,
-        GE: 1,
-        US: 1,
-        CA: 1
-      }
-    })
-  }
-}
+export const GET_VISITED_COUNTRIES = countries => ({
+  type: "GET_VISITED_COUNTRIES",
+  payload: countries
+});
